25b1f47414c0a9642768cda82b8926cd7e43622c
n1c0201 <56902710+n1c0201@users.noreply.github.com>
2020-01-15 15:07:28 +0700

    Add files via upload

---
+++ cardImage.py
@@ -0,0 +1,164 @@
+import pygame
+def getCardImage(slot, index):
+    if slot[index].suit == 2:
+        if slot[index].value == 2:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\2C.png')
+
+        if slot[index].value == 3:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\3C.png')
+
+        if slot[index].value == 4:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\4C.png')
+
+        if slot[index].value == 5:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\5C.png')
+
+        if slot[index].value == 6:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\6C.png')
+
+        if slot[index].value == 7:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\7C.png')
+
+        if slot[index].value == 8:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\8C.png')
+
+        if slot[index].value == 9:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\9C.png')
+
+        if slot[index].value == 10:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\10C.png')
+
+        if slot[index].value == 14:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\JC.png')
+
+        if slot[index].value == 12:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\QC.png')
+
+        if slot[index].value == 13:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\KC.png')
+
+        if slot[index].value == 11:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\AC.png')
+
+    if slot[index].suit == 0:
+        if slot[index].value == 2:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\2S.png')
+
+        if slot[index].value == 3:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\3S.png')
+
+        if slot[index].value == 4:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\4S.png')
+
+        if slot[index].value == 5:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\5S.png')
+
+        if slot[index].value == 6:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\6S.png')
+
+        if slot[index].value == 7:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\7S.png')
+
+        if slot[index].value == 8:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\8S.png')
+
+        if slot[index].value == 9:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\9S.png')
+
+        if slot[index].value == 10:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\10S.png')
+
+        if slot[index].value == 14:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\JS.png')
+
+        if slot[index].value == 12:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\QS.png')
+
+        if slot[index].value == 13:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\KS.png')
+
+        if slot[index].value == 11:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\AS.png')
+
+    if slot[index].suit == 1:
+        if slot[index].value == 2:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\2D.png')
+
+        if slot[index].value == 3:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\3D.png')
+
+        if slot[index].value == 4:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\4D.png')
+
+        if slot[index].value == 5:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\5D.png')
+
+        if slot[index].value == 6:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\6D.png')
+
+        if slot[index].value == 7:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\7D.png')
+
+        if slot[index].value == 8:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\8D.png')
+
+        if slot[index].value == 9:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\9D.png')
+
+        if slot[index].value == 14:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\10D.png')
+
+        if slot[index].value == 11:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\JD.png')
+
+        if slot[index].value == 12:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\QD.png')
+
+        if slot[index].value == 13:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\KD.png')
+
+        if slot[index].value == 11:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\AD.png')
+
+    if slot[index].suit == 3:
+        if slot[index].value == 2:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\2H.png')
+
+        if slot[index].value == 3:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\3H.png')
+
+        if slot[index].value == 4:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\4H.png')
+
+        if slot[index].value == 5:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\5H.png')
+
+        if slot[index].value == 6:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\6H.png')
+
+        if slot[index].value == 7:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\7H.png')
+
+        if slot[index].value == 8:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\8H.png')
+
+        if slot[index].value == 9:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\9H.png')
+
+        if slot[index].value == 10:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\10H.png')
+
+        if slot[index].value == 14:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\JH.png')
+
+        if slot[index].value == 12:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\QH.png')
+
+        if slot[index].value == 13:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\KH.png')
+
+        if slot[index].value == 11:
+            image = pygame.image.load(r'C:\Users\Nicholas Arthur\Desktop\python\Final Project\AH.png')
+    image = pygame.transform.scale(image, (168, 252))
+    return image
+
